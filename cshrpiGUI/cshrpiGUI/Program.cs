401bebf65b85917a7fc19efcfecf1c838ad0b456
Insoo Kim <insoo@hotmail.com>
2025-04-15 14:17:33 +0900

    GUI hello world for RPi

---
+++ cshrpiGUI/cshrpiGUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWorldApp
+{
+    public class HelloWorldForm : Form
+    {
+        public HelloWorldForm()
+        {
+            Text = "Hello World GUI";
+            Button button = new Button() { Text = "Click Me!", Dock = DockStyle.Fill };
+            button.Click += (sender, e) => MessageBox.Show("Hello, World!");
+            Controls.Add(button);
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new HelloWorldForm());
+        }
+    }
+}
